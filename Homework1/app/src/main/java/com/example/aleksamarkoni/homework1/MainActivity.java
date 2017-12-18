1a97c2576c308eb09772e4a19edc409b771e302d
aleksamarkoni <aleksamarkoni@gmail.com>
2017-12-18 20:04:52 +0100

    Dodao sam kod da nadjem textview

--- Homework1/app/src/main/java/com/example/aleksamarkoni/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/aleksamarkoni/homework1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aleksamarkoni.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO create variable and use findViewById to assign the TextView to it. That TextView should show the current state of the counter.
+        TextView countTextView = findViewById(R.id.counter_text_view);
 
         //TODO Create a variable that will point to the increment button
         //TODO use findViewById to fill that variable so it points to the Increment button
