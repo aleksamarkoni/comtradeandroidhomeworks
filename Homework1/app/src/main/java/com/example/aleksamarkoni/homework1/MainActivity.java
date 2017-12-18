5ca0fb55623f0340ce453edf3df841f4aea5d4d8
Aleksandar Markovic <aleksamarkoni@gmail.com>
2017-12-18 17:26:21 +0100

    Initial commit

---
+++ Homework1/app/src/main/java/com/example/aleksamarkoni/homework1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.aleksamarkoni.homework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO create the int counter variable that will store the current counter state
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO create variable and use findViewById to assign the TextView to it. That TextView should show the current state of the counter.
+
+        //TODO Create a variable that will point to the increment button
+        //TODO use findViewById to fill that variable so it points to the Increment button
+        //TODO Register the onClick listener for the IncrementButton
+        //TODO inside the listener, update the counter variable, and also update the TextView to display the new counter value
+
+        //TODO Create a variable that will point to the decrement button
+        //TODO use findViewById to fill that variable so it points to the Decrement button
+        //TODO Register the onClick listener for the DecrementButton
+        //TODO inside the listener, update the counter variable, and also update the TextView to display the new counter value
+
+    }
+}
