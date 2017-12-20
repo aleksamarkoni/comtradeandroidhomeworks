26f69a86b6799b68b11d182a5a477ebfb37c61d3
Miljannn <vujsicmiljan00@gmail.com>
2017-12-20 15:44:55 +0100

    adasd

--- Homework1/app/src/main/java/com/example/aleksamarkoni/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/aleksamarkoni/homework1/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.aleksamarkoni.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO create the int counter variable that will store the current counter state
+
+    TextView countTxtVju = findViewById(R.id.txt_vju);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
