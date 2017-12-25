b13722d727c963ca1c7330c31190bc24e0011880
Aleksandar Markovic <aleksamarkoni@gmail.com>
2017-12-25 20:11:29 +0100

    Dodao dva automobila

--- AutoPerionica/app/src/main/java/com/example/code/autoperionica/MainActivity.java
+++ AutoPerionica/app/src/main/java/com/example/code/autoperionica/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.code.autoperionica;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Auto> listOfCars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeCoupleOfCars();
+    }
+
+    private void makeCoupleOfCars() {
+        listOfCars = new ArrayList<>();
+        Auto auto = new Auto("Mercedes", "bg-007-jb", 70, 3);
+        listOfCars.add(auto);
+        auto = new Auto("Honda", "bg-1169-nt", 50, 4);
+        listOfCars.add(auto);
     }
 }
