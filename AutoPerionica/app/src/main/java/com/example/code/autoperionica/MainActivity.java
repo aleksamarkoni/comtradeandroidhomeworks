0671e44627df0d25bec33815eb78886e7845a9ad
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-01-03 21:06:23 +0100

    Change theme and add toolbar and fab button

--- AutoPerionica/app/src/main/java/com/example/code/autoperionica/MainActivity.java
+++ AutoPerionica/app/src/main/java/com/example/code/autoperionica/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.code.autoperionica;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         makeCoupleOfCars();
 
@@ -23,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         CarWashAdapter carWashAdapter = new CarWashAdapter(listOfCars);
         recyclerView.setAdapter(carWashAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        }).show();
+            }
+        });
     }
 
     private void makeCoupleOfCars() {
