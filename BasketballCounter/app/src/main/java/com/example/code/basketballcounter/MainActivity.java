8d3f16733048a56bfc91854b21d9da16ee2157e6
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-01-17 21:02:44 +0100

    First commit tictactoe

--- BasketballCounter/app/src/main/java/com/example/code/basketballcounter/MainActivity.java
+++ BasketballCounter/app/src/main/java/com/example/code/basketballcounter/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.code.basketballcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView teamAScore;
+    private Button teamAThreePoints;
+
+    private int currentTeamAScore;
+    private int currentTeamBScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teamAScore = findViewById(R.id.team_a_score);
+        teamAThreePoints = findViewById(R.id.team_a_plus_three);
+        teamAThreePoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String score = teamAScore.getText().toString();
+//                Integer scoreInt = Integer.parseInt(score);
+//                scoreInt += 3;
+//                teamAScore.setText(scoreInt + "");
+                currentTeamAScore += 3;
+                teamAScore.setText(currentTeamAScore + "");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("team_a_score", currentTeamAScore);
+        outState.putInt("team_b_score", currentTeamBScore);
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentTeamAScore = savedInstanceState.getInt("team_a_score");
+        currentTeamBScore = savedInstanceState.getInt("team_b_score");
+
+        teamAScore.setText(currentTeamAScore + "");
     }
 }
---
+++ SimpleTicTacToe/app/src/main/java/com/example/code/simpletictactoe/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.code.simpletictactoe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int buttons[] = {
+            R.id.b_1_1, R.id.b_1_2, R.id.b_1_3,
+            R.id.b_2_1, R.id.b_2_2, R.id.b_2_3,
+            R.id.b_3_1, R.id.b_3_2, R.id.b_3_3
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = findViewById(buttons[i]);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this,
+                            "Pritisnuo button " + view.getId(),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+        }
+    }
+}
