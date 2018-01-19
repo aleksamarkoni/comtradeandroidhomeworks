937fcf08b68e050c7c05813ebf0f8387c3be0e46
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-01-19 20:56:17 +0100

    Dodao kod za izlaz slika

--- SimpleTicTacToe/app/src/main/java/com/example/code/simpletictactoe/MainActivity.java
+++ SimpleTicTacToe/app/src/main/java/com/example/code/simpletictactoe/MainActivity.java
@@ -13,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
             R.id.b_2_1, R.id.b_2_2, R.id.b_2_3,
             R.id.b_3_1, R.id.b_3_2, R.id.b_3_3
     };
+    private ButtonState[][] buttonStates = {
+            {ButtonState.EMPTY, ButtonState.EMPTY, ButtonState.EMPTY},
+            {ButtonState.EMPTY, ButtonState.EMPTY, ButtonState.EMPTY},
+            {ButtonState.EMPTY, ButtonState.EMPTY, ButtonState.EMPTY}
+    };
+
+    private Player currentPlayer = Player.IKS_PLAYER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +30,91 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(MainActivity.this,
-                            "Pritisnuo button " + view.getId(),
-                            Toast.LENGTH_SHORT)
-                            .show();
+                    buttonClick(view);
                 }
             });
         }
     }
+
+    private void buttonClick(View view) {
+        switch (view.getId()) {
+            case R.id.b_1_1:
+                ispisiToast("prvi red, prvo dugme");
+                odigraPotez(view, 0, 0);
+                break;
+            case R.id.b_1_2:
+                ispisiToast("prvi red, drugo dugme");
+                odigraPotez(view, 0, 1);
+                break;
+            case R.id.b_1_3:
+                ispisiToast("prvi red, trece dugme");
+                odigraPotez(view, 0, 2);
+                break;
+            case R.id.b_2_1:
+                ispisiToast("drugi red, prvo dugme");
+                odigraPotez(view, 1, 0);
+                break;
+            case R.id.b_2_2:
+                ispisiToast("drugi red, drugo dugme");
+                odigraPotez(view, 1, 1);
+                break;
+            case R.id.b_2_3:
+                ispisiToast("drugi red, trece dugme");
+                odigraPotez(view, 1, 2);
+                break;
+            case R.id.b_3_1:
+                ispisiToast("treci red, prvo dugme");
+                odigraPotez(view, 2, 0);
+                break;
+            case R.id.b_3_2:
+                ispisiToast("treci red, drugo dugme");
+                odigraPotez(view, 2, 1);
+                break;
+            case R.id.b_3_3:
+                ispisiToast("treci red, trece dugme");
+                odigraPotez(view, 2, 2);
+                break;
+        }
+    }
+
+    private void odigraPotez(View button, int row, int col) {
+        switch (buttonStates[row][col]) {
+            case EMPTY:
+                switch (currentPlayer) {
+                    case IKS_PLAYER:
+                        button.setBackgroundResource(R.drawable.iks_background);
+                        buttonStates[row][col] = ButtonState.IKS;
+                        currentPlayer = Player.OKS_PLAYER;
+                        break;
+                    case OKS_PLAYER:
+                        button.setBackgroundResource(R.drawable.oks_background);
+                        buttonStates[row][col] = ButtonState.OKS;
+                        currentPlayer = Player.IKS_PLAYER;
+                        break;
+                }
+
+                break;
+//            case IKS:
+//                button.setBackgroundResource(R.drawable.oks_background);
+//                buttonStates[row][col] = ButtonState.OKS;
+//                break;
+//            case OKS:
+//                button.setBackgroundColor(getResources().getColor(R.color.white));
+//                buttonStates[row][col] = ButtonState.EMPTY;
+//                break;
+
+        }
+    }
+
+    private void ispisiToast(String s) {
+        Toast.makeText(MainActivity.this,
+                s,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    public enum ButtonState {EMPTY, IKS, OKS}
+
+    public enum Player {IKS_PLAYER, OKS_PLAYER}
+
 }
