fd76f912bf6ea1b7c17fd49093c837d361a0e280
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-01-24 20:24:42 +0100

    ADd a code to open a new activity

--- ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/TodoListActivity.java
+++ ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/TodoListActivity.java
@@ -1,8 +1,8 @@
 package com.example.code.comtradetodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -35,10 +35,14 @@ public class TodoListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openAddEditActivity();
             }
         });
+    }
+
+    private void openAddEditActivity() {
+        Intent intent = new Intent(this, AddEditTodoActivity.class);
+        startActivity(intent);
     }
 
 }
