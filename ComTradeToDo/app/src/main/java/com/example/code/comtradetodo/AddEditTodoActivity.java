3b447943030b781604adcd66647c20f857115f63
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-01-26 21:10:04 +0100

    Add a new code

--- ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/AddEditTodoActivity.java
+++ ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/AddEditTodoActivity.java
@@ -1,11 +1,13 @@
 package com.example.code.comtradetodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class AddEditTodoActivity extends AppCompatActivity {
 
@@ -16,12 +18,23 @@ public class AddEditTodoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final TextView textView = findViewById(R.id.edit_text_add_edit_activity_title_text_view);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                CharSequence todoTitle = textView.getText();
+                if (todoTitle == null) {
+                    Snackbar.make(view, "Nisi nista ni uneo", Snackbar.LENGTH_SHORT).show();
+                } else if (todoTitle.length() == 0) {
+                    Snackbar.make(view, "Prazan text", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent();
+                    intent.putExtra("todoTitle", todoTitle.toString());
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
             }
         });
     }
--- ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/TodoListActivity.java
+++ ComTradeToDo/app/src/main/java/com/example/code/comtradetodo/TodoListActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -15,8 +16,13 @@ import java.util.List;
 
 public class TodoListActivity extends AppCompatActivity {
 
+    private static final String TODO_LIST_BUNDLE_KEY = "todo_list_bundle_key";
+    private static final int ADD_EDIT_ACTIVITY_REQUEST_CODE = 10;
+    private static final String TAG = TodoListActivity.class.getSimpleName();
+
     private ArrayList<Todo> todoList;
     private RecyclerView recyclerView;
+    private TodoAdapter todoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +37,22 @@ public class TodoListActivity extends AppCompatActivity {
             todoList = new ArrayList<>();
             todoList.add(new Todo("kolica", true));
             todoList.add(new Todo("sok", true));
-            todoList.add(new Todo("plazma"));
-            todoList.add(new Todo("guarana"));
-            todoList.add(new Todo("maslac"));
-            todoList.add(new Todo("mleko", true));
-            todoList.add(new Todo("brasno"));
-            todoList.add(new Todo("sapun"));
-            todoList.add(new Todo("kolica 1"));
-            todoList.add(new Todo("sok 1", true));
-            todoList.add(new Todo("plazma 1"));
-            todoList.add(new Todo("guarana 1", true));
-            todoList.add(new Todo("maslac 1"));
-            todoList.add(new Todo("mleko 1", true));
-            todoList.add(new Todo("brasno 1", true));
-            todoList.add(new Todo("sapun 1"));
-            recyclerView.setAdapter(new TodoAdapter(todoList));
+//            todoList.add(new Todo("plazma"));
+//            todoList.add(new Todo("guarana"));
+//            todoList.add(new Todo("maslac"));
+//            todoList.add(new Todo("mleko", true));
+//            todoList.add(new Todo("brasno"));
+//            todoList.add(new Todo("sapun"));
+//            todoList.add(new Todo("kolica 1"));
+//            todoList.add(new Todo("sok 1", true));
+//            todoList.add(new Todo("plazma 1"));
+//            todoList.add(new Todo("guarana 1", true));
+//            todoList.add(new Todo("maslac 1"));
+//            todoList.add(new Todo("mleko 1", true));
+//            todoList.add(new Todo("brasno 1", true));
+//            todoList.add(new Todo("sapun 1"));
+            todoAdapter = new TodoAdapter(todoList);
+            recyclerView.setAdapter(todoAdapter);
         }
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -60,26 +67,43 @@ public class TodoListActivity extends AppCompatActivity {
 
     private void openAddEditActivity() {
         Intent intent = new Intent(this, AddEditTodoActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, ADD_EDIT_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ADD_EDIT_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String todoTitle = data.getStringExtra("todoTitle");
+                Log.d(TAG, "stigao mi je resultat: " + todoTitle);
+                Todo todo = new Todo(todoTitle);
+                todoList.add(todo);
+                todoAdapter.notifyItemInserted(todoList.size() - 1);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList("lista", todoList);
+        outState.putParcelableArrayList(TODO_LIST_BUNDLE_KEY, todoList);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        todoList = savedInstanceState.getParcelableArrayList("lista");
+        todoList = savedInstanceState.getParcelableArrayList(TODO_LIST_BUNDLE_KEY);
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         if (savedInstanceState != null) {
-            recyclerView.setAdapter(new TodoAdapter(todoList));
+            todoAdapter = new TodoAdapter(todoList);
+            recyclerView.setAdapter(todoAdapter);
         }
     }
 }
