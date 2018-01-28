1d5a425d0508030bc1ea31ec05cf8d74e3061436
Milan Milanovic <milan.milanovic@Ipsos.com>
2018-01-28 18:10:35 +0100

    dodati ly razni i dugmici

--- Domaci/app/src/main/java/com/example/code/domaci/MainActivity.java
+++ Domaci/app/src/main/java/com/example/code/domaci/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.code.domaci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_reltest);
+
+
+    Button button = findViewById(R.id.add_max);
+    button.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Toast toast;
+            Toast.makeText(getApplicationContext(), "text", Toast.LENGTH_SHORT).show();
+
+        }
+    });
     }
 }
