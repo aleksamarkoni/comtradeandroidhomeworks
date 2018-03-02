d1e959d212d428cf8d367aa6150f8d89fdb64ecc
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-03-02 19:27:18 +0100

    Add a new code

--- YambJava/src/Hand.java
+++ YambJava/src/Hand.java
@@ -7,13 +7,16 @@ public class Hand {
     private int numberOfDiceces;
     private int numberOfSides;
     Random random;
+    Bacanje bacanje;
+
 
     public Hand() {
         this.numberOfDiceces = 6;
         this.numberOfSides = 6;
         hand = new int[numberOfDiceces];
         rez = new int[numberOfSides];
-        random = new Random(400);
+        random = new Random();
+        bacanje = Bacanje.POCETAK;
     }
 
     public Hand(int numberOfDiceces, int numberOfSides) {
@@ -21,17 +24,40 @@ public class Hand {
         this.numberOfSides = numberOfSides;
         hand = new int[numberOfDiceces];
         rez = new int[numberOfSides];
-        random = new Random(400);
+        random = new Random();
+        bacanje = Bacanje.POCETAK;
     }
 
-    public void nextHand() {
+    public void nextThrow() {
+        switch (bacanje) {
+            case POCETAK:
+                generisiNoveKockice();
+                izbrojKockice();
+                bacanje = Bacanje.PRVO_BACANJE;
+                break;
+            case PRVO_BACANJE:
+                generisiNoveKockice();
+                izbrojKockice();
+                bacanje = Bacanje.DRUGO_BACANJE;
+                break;
+            case DRUGO_BACANJE:
+                generisiNoveKockice();
+                izbrojKockice();
+                bacanje = Bacanje.TRECE_BACANJE;
+                break;
+            case TRECE_BACANJE:
+                System.out.println("Ne moze, ruka je gotova, mora reset");
+                break;
+        }
+    }
+
+    private void generisiNoveKockice() {
         for (int i = 0; i < numberOfDiceces; i++) {
             hand[i] = 1 + random.nextInt(numberOfSides);
         }
-        izbrojKockice();
     }
 
-    public void nextHand(int izborKockica[]) {
+    public void nextThrow(int izborKockica[]) {
         for (int i = 0; i < izborKockica.length; i++) {
             int kockicaKojuMenjamo = izborKockica[i]-1;
             hand[kockicaKojuMenjamo] = 1 + random.nextInt(numberOfSides);
@@ -40,15 +66,39 @@ public class Hand {
     }
 
     public int yumb() {
-        return daLiImaBarem(5);
+        return izracunajSkor(daLiImaBarem(5), 5);
     }
 
     public int poker() {
-        return daLiImaBarem(4);
+        return izracunajSkor(daLiImaBarem(4), 4);
     }
 
     public int triling() {
-        return daLiImaBarem(3);
+        return izracunajSkor(daLiImaBarem(3), 3);
+    }
+
+    public boolean kenta() {
+        return (rez[1] >= 1 && rez[2] >= 1 && rez[3] >= 1 && rez[4] >= 1) && (rez[0] >= 1 || rez[5] >= 1);
+    }
+
+    public int full() {
+        int maxTrojke = daLiImaBarem(3);
+        if (maxTrojke == -1) {
+            return -1;
+        }
+        int maxDvojke = -1;
+        for (int i = numberOfSides - 1; i >= 0; i--) {
+            if (i != (maxTrojke - 1)) {
+                if (rez[i] >= 2) {
+                    maxDvojke = (i + 1);
+                    break;
+                }
+            }
+        }
+        if (maxDvojke == -1) {
+            return -1;
+        }
+        return maxTrojke * 3 + maxDvojke * 2;
     }
 
     public int max() {
@@ -76,10 +126,17 @@ public class Hand {
     private int daLiImaBarem(int x) {
         for (int i = numberOfSides - 1; i >= 0; i--) {
             if (rez[i] >= x) {
-                return (i + 1) * x;
+                return (i + 1);
             }
         }
         return -1;
+    }
+
+    private int izracunajSkor(int kockica, int kolikoIhIma) {
+        if (kockica == -1) {
+            return -1;
+        }
+        return kockica * kolikoIhIma;
     }
 
     private void izbrojKockice() {
@@ -113,13 +170,34 @@ public class Hand {
         if (poker != -1) {
             stringBuilder.append("poker:").append(poker).append(", ");
         }
+
+        int full = full();
+        if (full != -1) {
+            stringBuilder.append("full:").append(full).append(", ");
+        }
+
+        if (kenta()) {
+            stringBuilder.append("kenta, ");
+        }
         int triling = triling();
         if (triling != -1) {
             stringBuilder.append("triling:").append(triling).append(", ");
         }
         stringBuilder.append("max:").append(max()).append(", ");
         stringBuilder.append("min:").append(min()).append(", ");
+        stringBuilder.append("Stanje: ").append(bacanje.toString());
         stringBuilder.append("}");
         return stringBuilder.toString();
+    }
+
+    public void zadrziKockice() {
+
+    }
+
+    enum Bacanje {
+        POCETAK,
+        PRVO_BACANJE,
+        DRUGO_BACANJE,
+        TRECE_BACANJE
     }
 }
--- YambJava/src/Main.java
+++ YambJava/src/Main.java
@@ -1,14 +1,15 @@
-import java.util.Random;
-
 public class Main {
 
     public static void main(String args[]) {
         Hand hand = new Hand();
-        hand.nextHand();
-        System.out.println(hand);
-        hand.nextHand(new int[]{1, 2, 5, 6});
-        System.out.println(hand);
-        hand.nextHand(new int[]{1, 2, 6});
+        hand.nextThrow(); //prvo bacanje
+        //kraj
+        hand.zadrziKockice();
+        hand.nextThrow(); //drugo bacanje
+        //kraj
+        hand.zadrziKockice();
+        //hand.nextThrow(); // trece bacanje
+        //kraj
         System.out.println(hand);
     }
 }
