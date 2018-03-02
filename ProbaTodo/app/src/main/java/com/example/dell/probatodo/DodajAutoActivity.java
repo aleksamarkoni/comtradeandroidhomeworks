c1a2fe09cdfeddf0da3f5d964fc1d573dfd4c405
Miljannn <vujsicmiljan00@gmail.com>
2018-03-02 01:59:09 +0100

    proba

--- ProbaTodo/app/src/main/java/com/example/dell/probatodo/DodajAutoActivity.java
+++ ProbaTodo/app/src/main/java/com/example/dell/probatodo/DodajAutoActivity.java
@@ -1,34 +1,68 @@
 package com.example.dell.probatodo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DodajAutoActivity extends AppCompatActivity {
 
     public static final String AUTO_INTENT_KEY = "auto_intent_key";
-    ImageView imageView;
+    private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 10;
+    private static final int REQUEST_IMAGE_CAPTURE = 5;
+    ImageView imageVieww;
     TextView imeVlasnika;
     TextView registracija;
     CheckBox pranje;
     CheckBox voskiranje;
     Button dodajAuto;
+    ImageButton imageButton;
+    ConstraintLayout layout;
+    private String mCurrentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_auto);
 
-        imageView = findViewById(R.id.imageView);
+        imageVieww = findViewById(R.id.imageView);
         imeVlasnika = findViewById(R.id.ime_vlasnika);
         registracija = findViewById(R.id.registracija);
         pranje = findViewById(R.id.pranje);
         voskiranje = findViewById(R.id.voskiranje);
+        layout = findViewById(R.id.dodaj_auto_layout);
+        imageButton = findViewById(R.id.image_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkForPermision();
+            }
+        });
         dodajAuto = findViewById(R.id.dodaj_auto_button);
         dodajAuto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +81,152 @@ public class DodajAutoActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //imageView.setImageBitmap(imageBitmap);
+            new DecodePictureAsyncTask(imageVieww).execute(mCurrentPhotoPath);
+        }
+    }
+
+    private void checkForPermision() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            takeAPicture();
+        } else {
+            requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Snackbar.make(layout, R.string.obavestenje,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+
+                    ActivityCompat.requestPermissions(DodajAutoActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+            }).show();
+        } else {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    private void takeAPicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.probaprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //writeToFile();
+                takeAPicture();
+            }
+        } else {
+            Snackbar.make(layout, "Write permission request was denied.",
+                    Snackbar.LENGTH_SHORT).
+                    show();
+        }
+    }
+
+    private static class DecodePictureAsyncTask extends AsyncTask<String, Void, Bitmap> {
+
+        ImageView imageView;
+        ProgressBar progressBar;
+
+        public DecodePictureAsyncTask(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.INVISIBLE);
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String fileName = urls[0];
+            int targetW = imageView.getWidth();
+            int targetH = imageView.getHeight();
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(fileName, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            Bitmap bitmap = BitmapFactory.decodeFile(fileName, bmOptions);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            progressBar.setVisibility(View.INVISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
